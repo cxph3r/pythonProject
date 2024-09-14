afa11fd5f5d77c11d57e647427d7448648771745
cxph3r <bsrikarthikeya@gmail.com>
2024-09-15 02:00:21 +0530

    edit sumOfTwoNumbers.py

--- sumOfTwoNumbers.py
+++ sumOfTwoNumbers.py
@@ -1,8 +1,10 @@
+# Program: Finding sum of two numbers using user-input values.
+
 num1 = input("Enter First Number: ")
 num2 = input("Enter Second Number: ")
 
 add = float(num1) + float(num2)
 
-#print("The Sum of Two Integers are: ", add)
+# print("The Sum of Two Integers are: ", add)
 
-print("The Sum Of {0} + {1} = {2}" .format(num1, num2, add))
+print("The Sum Of {0} + {1} = {2}".format(num1, num2, add))
